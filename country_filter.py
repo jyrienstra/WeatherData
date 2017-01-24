c4f79e0a16a3b6e81e16eccf33faef9db2b28371
Matthijs Bonnema <msbonnema@gmail.com>
2017-01-24 14:09:17 +0100

    Updated

--- country_filter.py
+++ country_filter.py
@@ -1,12 +1,27 @@
-serbia_list = []
+import os
 
-file = open("data/country_data.csv", "r")
-for line in file:
-    line_list = line.split(',')
-    if "SERBIA" in line_list[1]:
-        serbia_list.append((line_list[0]).strip('"'))
-print(serbia_list)
+option = input("Do you want to empty the country filter? (yes/no)\n")
+if option == "yes":
+    os.remove("data/filter.csv")
 
-filter_file = open("data/filter.csv", 'w')
-for line in serbia_list:
-    filter_file.write("{}\n".format(line))
+option = input("Do you want to add a country to the filter? (yes/no)\n")
+if option == "yes":
+    country = input("What country do you want to add? (cancel to cancel)\n")
+    if country == "cancel":
+        print("Goodbye!")
+        exit(0)
+    else:
+        serbia_list = []
+
+        file = open("data/country_data.csv", "r")
+        for line in file:
+            line_list = line.split(',')
+            if country.upper() in line_list[1]:
+                serbia_list.append((line_list[0]).strip('"'))
+
+        print("These stations will be added:")
+
+        filter_file = open("data/filter.csv", 'w')
+        for line in serbia_list:
+            print(line)
+            filter_file.write("{}\n".format(line))
