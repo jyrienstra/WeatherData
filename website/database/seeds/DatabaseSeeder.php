1da2d00b02175ab08d61e50dcdbb4027509bd560
DDJager <danny-1995@live.nl>
2017-01-27 18:46:43 +0100

    Added a seeder for creating test users

--- website/database/seeds/DatabaseSeeder.php
+++ website/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+    	// Run the database seed for the UsersTableSeeder class
+        $this->call(UsersTableSeeder::class);
     }
 }
